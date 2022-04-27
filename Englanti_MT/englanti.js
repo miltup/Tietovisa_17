e10bd048cfce6f6aa3b92681e2ec8fd7c900337d
Milla Tuppurainen <you@example.com>
2022-04-27 21:10:44 +0300

    lisätty headeriin kuva

--- Englanti_MT/englanti.js
+++ Englanti_MT/englanti.js
@@ -1,18 +1,18 @@
 
 // Milla Tuppurainen
 
-// VARIABLES
+// Correct answers
     let question1Answer1Radio = document.querySelector("#flexRadioDefault1");
     let question2Answer2Radio = document.querySelector("#flexRadioDefault6");
     let question3Answer2Radio = document.querySelector("#flexRadioDefault10");
     let question4Answer2Radio = document.querySelector("#flexRadioDefault14");
     let question5Answer4Radio = document.querySelector("#flexRadioDefault20");
 
+// Amount of answered questions
     let amountOfAnsweredQuestions = 0;
     let answeredQuestions = document.querySelector("#answeredQuestions");
 
 // CHECKING THE ANSWERS
-    // Question 1, checking if the answer was correct, telling which answer is correct and telling why it was correct/wrong.
     function answerCheck1() {
         let argument1 = document.querySelector("#argument1");
 
@@ -22,25 +22,26 @@
             argument1.innerHTML += "Vastasit oikein!" + "<br>" + "<br>";
         } else {
             argument1.innerHTML += "Vastasit väärin." + "<br>" + "<br>";
-            document.getElementById("question1Answer1").style.backgroundColor = "#AB9FCC";
+            document.getElementById("question1Answer1").style.backgroundColor = "#81F4E1";
         }
         
-        argument1.innerHTML += "Turn right = käänny oikealle" + "<br>" + "Follow the street to the end = seuraa tietä päähän saakka" + "<br>" + "Turn left = käänny vasemmalle";
+        argument1.innerHTML += "Turn right = käänny oikealle" + "<br>" + 
+        "Follow the street to the end = seuraa tietä päähän saakka" + "<br>" + 
+        "Turn left = käänny vasemmalle";
         argument1.style.visibility = "visible";
 
-        // Question 1, telling the amount of answered questions
+        // The amount of answered questions
         amountOfAnsweredQuestions++;
         answeredQuestions.innerHTML = "Olet vastannut " + amountOfAnsweredQuestions + "/5 kysymykseen.";
         answeredQuestions.style.visibility = "visible";
 
-        // Question 1, disabling radio buttons after choice
+        // Disabling radio buttons after choice
         question1Answer1Radio.disabled = true;
         document.querySelector("#flexRadioDefault2").disabled = true;
         document.querySelector("#flexRadioDefault3").disabled = true;
         document.querySelector("#flexRadioDefault4").disabled = true;
     }
 
-    // Question 2, checking if the answer was correct, telling which answer is correct and telling why it was correct/wrong.
     function answerCheck2() {
         let argument2 = document.querySelector("#argument2");
 
@@ -50,25 +51,23 @@
             argument2.innerHTML += "Vastasit oikein!" + "<br>" + "<br>";
         } else {
             argument2.innerHTML += "Vastasit väärin." + "<br>" + "<br>";
-            document.getElementById("question2Answer2").style.backgroundColor = "#AB9FCC";
+            document.getElementById("question2Answer2").style.backgroundColor = "#81F4E1";
         }
 
-        argument2.innerHTML += "April = Huhtikuu" + "<br>" + "May = Toukokuu" + "<br>" + "November = Marraskuu" + "<br>" + "July = Heinäkuu"; 
+        argument2.innerHTML += "April = Huhtikuu" + "<br>" + "May = Toukokuu" + 
+        "<br>" + "November = Marraskuu" + "<br>" + "July = Heinäkuu"; 
         argument2.style.visibility = "visible";
 
-        // Question 2, telling the amount of answered questions
         amountOfAnsweredQuestions++;
         answeredQuestions.innerHTML = "Olet vastannut " + amountOfAnsweredQuestions + "/5 kysymykseen.";
         answeredQuestions.style.visibility = "visible";
 
-        // Question 2, disabling radio buttons after choice
         document.querySelector("#flexRadioDefault5").disabled = true;
         question2Answer2Radio.disabled = true;
         document.querySelector("#flexRadioDefault7").disabled = true;
         document.querySelector("#flexRadioDefault8").disabled = true;
     }
 
-    // Question 3, checking if the answer was correct, telling which answer is correct and telling why it was correct/wrong.
     function answerCheck3() {
         let argument3 = document.querySelector("#argument3");
 
@@ -78,25 +77,23 @@
             argument3.innerHTML += "Vastasit oikein" + "<br>" + "<br>";
         } else {
             argument3.innerHTML += "Vastasit väärin." + "<br>" + "<br>";
-            document.getElementById("question3Answer2").style.backgroundColor = "#AB9FCC";
+            document.getElementById("question3Answer2").style.backgroundColor = "#81F4E1";
         }
         
-        argument3.innerHTML += "Mathematics = matikka" + "<br>" + "Environmental studies = ympäristötieto" + "<br>" + "History = historia" + "<br>" + "Biology = biologia" + "<br>" + "Art = kuvaamataito"; 
+        argument3.innerHTML += "Mathematics = matikka" + "<br>" + "Environmental studies = ympäristötieto" + 
+        "<br>" + "History = historia" + "<br>" + "Biology = biologia" + "<br>" + "Art = kuvaamataito"; 
         argument3.style.visibility = "visible";
 
-        // Question 3, telling the amount of answered questions
         amountOfAnsweredQuestions++;
         answeredQuestions.innerHTML = "Olet vastannut " + amountOfAnsweredQuestions + "/5 kysymykseen.";
         answeredQuestions.style.visibility = "visible";
 
-        // Question 3, disabling radio buttons after choice
         document.getElementById("flexRadioDefault9").disabled = true;
         question3Answer2Radio.disabled = true;
         document.getElementById("flexRadioDefault11").disabled = true;
         document.getElementById("flexRadioDefault12").disabled = true;
     }
 
-    // Question 4, checking if the answer was correct, telling which answer is correct and telling why it was correct/wrong.
     function answerCheck4() {
         let argument4 = document.querySelector("#argument4");
 
@@ -106,25 +103,23 @@
             argument4.innerHTML += "Vastasit oikein!" + "<br>" + "<br>";
         } else {
             argument4.innerHTML += "Vastasit väärin." + "<br>" + "<br>";
-            document.getElementById("question4Answer2").style.backgroundColor = "#AB9FCC";
+            document.getElementById("question4Answer2").style.backgroundColor = "#81F4E1";
         }
         
-        argument4.innerHTML += "Between = välissä" + "<br>" + "Below = alapuolella" + "<br>" + "On top of = päällä" + "<br>" + "Next to = vieressä"; 
+        argument4.innerHTML += "Between = välissä" + "<br>" + "Below = alapuolella" + 
+        "<br>" + "On top of = päällä" + "<br>" + "Next to = vieressä"; 
         argument4.style.visibility = "visible";
 
-        // Question 4, telling the amount of answered questions
         amountOfAnsweredQuestions++;
         answeredQuestions.innerHTML = "Olet vastannut " + amountOfAnsweredQuestions + "/5 kysymykseen.";
         answeredQuestions.style.visibility = "visible";
 
-        // Question 4, disabling radio buttons after choice
         document.getElementById("flexRadioDefault13").disabled = true;
         question4Answer2Radio.disabled = true;
         document.getElementById("flexRadioDefault15").disabled = true;
         document.getElementById("flexRadioDefault16").disabled = true;
     }
 
-    // Question 5, checking if the answer was correct, telling which answer is correct and telling why it was correct/wrong.
     function answerCheck5() {
         let argument5 = document.querySelector("#argument5");
 
@@ -134,32 +129,29 @@
             argument5.innerHTML += "Vastasit oikein!" + "<br>" + "<br>";
         } else {
             argument5.innerHTML += "Vastasit väärin." + "<br>" + "<br>";
-            document.getElementById("question5Answer4").style.backgroundColor = "#AB9FCC";
+            document.getElementById("question5Answer4").style.backgroundColor = "#81F4E1";
         }
         
-        argument5.innerHTML += "Volleyball = lentopallo" + "<br>" + "Skiing = hiihto" + "<br>" + "Basketball = koripallo" + "<br>" + "Football = jalkapallo";
+        argument5.innerHTML += "Volleyball = lentopallo" + "<br>" + "Skiing = hiihto" + 
+        "<br>" + "Basketball = koripallo" + "<br>" + "Football = jalkapallo";
         argument5.style.visibility = "visible";
 
-        // Question 5, telling the amount of answered questions
         amountOfAnsweredQuestions++;
         answeredQuestions.innerHTML = "Olet vastannut " + amountOfAnsweredQuestions + "/5 kysymykseen.";
         answeredQuestions.style.visibility = "visible";
 
-        // Question 5, disabling radio buttons after choice
         document.getElementById("flexRadioDefault17").disabled = true;
         document.getElementById("flexRadioDefault18").disabled = true;
         document.getElementById("flexRadioDefault19").disabled = true;
         question5Answer4Radio.disabled = true;
     }
 
-//POINTS
-
     // Prizes
-    let coins = '<img src="/Images/coins.png" width=50%>';
-    let emerald = '<img src="/Images/emerald.png" width=50%>'; 
-    let ruby = '<img src="/Images/ruby.png" width=50%>';
-    let diamond = '<img src="/Images/diamond.png" width=50%>';
-    let treasure = '<img src="/Images/chest.png" width=50%>';
+    let coins = '<img src="../Images/coins.png" width=50%>';
+    let emerald = '<img src="../Images/emerald.png" width=50%>'; 
+    let ruby = '<img src="../Images/ruby.png" width=50%>';
+    let diamond = '<img src="../Images/diamond.png" width=50%>';
+    let treasure = '<img src="../Images/chest.png" width=50%>';
 
     // Calculating and returning the points
     let point = 0;
@@ -194,42 +186,36 @@
         document.getElementById("engbutton").disabled = true;
     } 
 
-    // Question 1, points
     function checkPoint1(point) {
         if (question1Answer1Radio.checked) {
             point++;
         } return point;
     }
 
-    // Question 2, points
     function checkPoint2(point) {
         if (question2Answer2Radio.checked) {
             point++;
         } return point;
     }
 
-    // Question 3, points
     function checkPoint3(point) {
         if (question3Answer2Radio.checked) {
             point++;
         } return point;
     }
 
-    // Question 4, points
     function checkPoint4(point) {
         if (question4Answer2Radio.checked) {
             point++;
         } return point;
     }
 
-    // Question 5, points
     function checkPoint5(point) {
         if (question5Answer4Radio.checked) {
             point++;
         } return point;
     }
 
-//BONUS QUESTION
     function checkBonus() {
         document.getElementById("bonusInput").innerHTML = "";
         document.getElementById("bonusresult").innerHTML = "";
